c095fdacc0f33139fd63d72f0111e0b31e6e1b79
WO-Nagano Tamawo <nagano.tamawo77@gmail.com>
2022-08-19 01:14:40 +0900

    APIとりあえず叩き終わった?

--- src/LoggedIn.js
+++ src/LoggedIn.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 
 const LoggedIn = (props) => {
 
-  const [posts, setPosts] = useState([]);
+  const [me, setMe] = useState([]);
+  const [artists, setAritists] = useState([]);
+  const [relatedArtists, setRelatedArtists] = useState([]);
 
   useEffect(() => {
+
     async function fetchData() {
+
       await fetch('https://api.spotify.com/v1/me', {
         method: 'GET',
         headers: {
@@ -13,10 +17,50 @@ const LoggedIn = (props) => {
         }
       }).then(res => res.json())
         .then(data => {
-          setPosts(data);
-          console.log(data);
+          setMe(data);
+          // console.log(data);
+        });
+
+      await fetch('https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=10&offset=0', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + props.accessToken
+        }
+
+      }).then(res => res.json())
+        .then(data => {
+          
+          setAritists(data);
+
+          let resultList = [];
+          let processes = [];
+          data.items.forEach(item => {
+            processes.push(
+              fetch('https://api.spotify.com/v1/artists/' + item.id + '/related-artists', 
+                { 
+                  method: 'GET', 
+                  headers: {Authorization: 'Bearer ' + props.accessToken}
+                }
+              )
+            );
+          });
+
+          // console.log(processes);
+
+          Promise.all(processes)
+            .then((res2 => Promise.all(res2.map(r => r.json()))))
+            .then((data2) => {
+              setRelatedArtists(data2);
+              // console.log(data);
+              // resultList = data;
+            })
+            // .catch(alert);
+
         });
     }
+
+    // console.log(relatedArtists);
+
     fetchData();
   }, []);
 
@@ -25,9 +69,33 @@ const LoggedIn = (props) => {
       <h1>Spotifyへログインしました</h1>
       <div>
         <ul>
-            {/* {console.log("aaa")} */}
-            <li>{posts.display_name}</li>
-            {posts.images ? <li><img src={posts.images[0].url}/></li> : <li>画像取得失敗</li>}
+            <li>{me.display_name}</li>
+            {me.images ? <li><img src={me.images[0].url}/></li> : <li>画像取得失敗</li>}
+            {
+              artists.items 
+                ? artists.items.map((item, index) => {
+                  return (
+                    <>
+                      <br />
+                      <br />
+                      <li key={item.id}>{item.name}</li>
+                      <li><img src={item.images[0].url}/></li>
+
+                      {
+                        relatedArtists.length != 0
+                          ? relatedArtists[index].artists.map((artist) => {
+                            return (
+                              <li>{artist.name}</li>
+                            )
+                          }) 
+                          : <li>関連アーティスト取得失敗</li>
+                      }
+
+                    </>
+                  );
+                })
+                : <li>取得失敗</li>
+            }
         </ul>
       </div>
     </div>
